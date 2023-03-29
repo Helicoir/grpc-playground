9ba8dcf0daf4423db489e59ef0f3588ea59432ed
Helicoir <ga.helicoir@gmail.com>
2023-03-29 10:16:26 +0900

    例に従ってサーバ&clientを立ち上げtsの型を吐き出させる

---
+++ src/books/index.ts
@@ -0,0 +1,10 @@
+export const bookData: { [x: string]: Record<string, any> } = {
+  book1: {
+    title: "Book1",
+    author: "Author1",
+  },
+  book2: {
+    title: "Book2",
+    author: "Author2",
+  },
+};
